2bd2b96092de55680fe5f4bfdb3514731d350c6f
Youssef Ezz-Eldeen <yousf2050@hotmail.com>
2022-05-19 14:24:52 +0300

    first project

--- dart_application_1/bin/person_visitas.dart
+++ dart_application_1/bin/person_visitas.dart
@@ -16,46 +16,53 @@ String printList() {
     CityClass(id: 4, name: 'Rafh', postCode: '00970'),
     CityClass(id: 5, name: 'Rafh', postCode: '00970'),
   ];
-  List<CityClass> listOfCitesVisits = [
-    CityClass(id: 2, name: 'Rafh', postCode: '00970'),
-    CityClass(id: 3, name: 'Rafh', postCode: '00970'),
-  ];
-  List<CityClass> listOfCitesVisits2 = [
-    CityClass(id: 2, name: 'Rafh', postCode: '00970'),
-    CityClass(id: 3, name: 'Rafh', postCode: '00970'),
-    CityClass(id: 5, name: 'Rafh', postCode: '00970'),
-  ];
+
   List<PersonClass> persons = [
     PersonClass(
         id: 1,
         name: "name",
         address: "address",
         gender: GenderEnum.male,
-        visits: listOfCitesVisits),
+        visits: listOfCites
+            .where((CityClass cityClass) =>
+                cityClass.id == 1 || (cityClass.id) == 5)
+            .toList()),
     PersonClass(
         id: 2,
         name: "name",
         address: "address",
         gender: GenderEnum.male,
-        visits: listOfCitesVisits),
+        visits: listOfCites
+            .where((CityClass cityClass) =>
+                cityClass.id == 2 || (cityClass.id) == 4)
+            .toList()),
     PersonClass(
         id: 3,
         name: "name",
         address: "address",
         gender: GenderEnum.male,
-        visits: listOfCitesVisits2),
+        visits: listOfCites
+            .where((CityClass cityClass) =>
+                cityClass.id == 3 || (cityClass.id) == 2)
+            .toList()),
     PersonClass(
         id: 4,
         name: "name",
         address: "address",
         gender: GenderEnum.male,
-        visits: listOfCitesVisits),
+        visits: listOfCites
+            .where((CityClass cityClass) =>
+                cityClass.id == 4 || (cityClass.id) == 3)
+            .toList()),
     PersonClass(
         id: 5,
         name: "name",
         address: "address",
         gender: GenderEnum.male,
-        visits: listOfCitesVisits2),
+        visits: listOfCites
+            .where((CityClass cityClass) =>
+                cityClass.id == 5 || (cityClass.id) == 3 || (cityClass.id == 4))
+            .toList()),
   ];
   int x = 0;
   for (PersonClass person in persons) {
